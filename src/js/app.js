3cf1af016ce1315f6800f8dc660d4243f121bbe6
Your Name <you@example.com>
2024-05-21 12:55:02 +0500

    init

---
+++ src/js/app.js
@@ -0,0 +1,39 @@
+import '../css/style.css'
+import imgGame from '../img/goblin.png'
+const fields = document.querySelectorAll(".fields");
+const img = document.createElement("img");
+
+
+
+let number = 0; 
+
+img.src = imgGame;
+fields[number].append(img);
+
+
+   function demo() { 
+    function getRandom(max) {
+    let randomA = Math.floor(Math.random() * max);
+    while (randomA === number) {
+      randomA = Math.floor(Math.random() * max);
+          } 
+          number = randomA; 
+          return number; 
+        } 
+        getRandom(16);
+        fields[number].append(img);
+    }
+    
+     let interval = setInterval(demo, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
