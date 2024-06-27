7d67af1d5db368ee343894edd77787c2bfa197ec
Your Name <you@example.com>
2024-06-27 14:19:46 +0500

    init

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,8 @@ import { gameClass } from "./clasess-game"
 
 const gamePlay = new gameClass();
 
-//gamePlay.init();
-//gamePlay.start();
+gamePlay.init();
+gamePlay.start();
 
 
 
--- src/js/clasess-game.js
+++ src/js/clasess-game.js
@@ -1,51 +1,54 @@
 import imgGame from '../img/goblin.png'
 
-
-
-
 export class gameClass {
-    constructor() {
-        this.dead = document.getElementById('dead');
-        this.lost = document.getElementById("lost");
-        this.fields = document.querySelectorAll(".fields");
-        this.number = 0; 
-       }
-    init() {
-     fields.forEach((el) => el.addEventListener("click", () => {
-      img.src = imgGame;
-      fields[this.number].append(img);
-       if(el.contains(imgGame)) {
-          this.dead.textContent++;
-        } else {
-          this.lost.textContent++;
-          if(this.dead === 10) {
-            alert("Вы победили");
-            this.dead = 0;
-            this.lost = 0;
-          }
-          if(this.lost === 5) {
-            alert("Вы проиграли");
-            this.dead = 0;
-            this.lost = 0;
-
-          }
+  constructor() {
+    this.dead = document.getElementById('dead');
+    this.lost = document.getElementById("lost");
+    this.fields = document.querySelectorAll(".fields");
+    this.img = document.createElement("img");
+    this.number = 0;
+  }
+  init() {
+    this.img.src = imgGame;
+    this.fields[this.number].append(this.img);
+    this.fields.forEach((el) => el.addEventListener("click", () => {
+      let imgGoblin = document.querySelector("img");
+      if (el.contains(imgGoblin)) {
+        this.dead.textContent++;
+        if (Number(this.dead.textContent) === 10) {
+          alert("Вы победили");
+          this.dead.textContent = 0;
+          this.lost.textContent = 0;
         }
-      }))
+      } else {
+        this.lost.textContent++;
+        if (Number(this.lost.textContent) === 5) {
+          alert("вы проиграли");
+          this.lost.textContent = 0;
+          this.dead.textContent = 0;
+        }
+      }
+    }))
+  }
+  start() {
+    let max = this.fields.length;
+    let getRandom = () => {
+      let randomA = Math.floor(Math.random() * max);
+      while (randomA === this.number) {
+        randomA = Math.floor(Math.random() * max);
+      }
+      this.number = randomA;
+      this.fields[this.number].append(this.img);
     }
-    start() {
-      function getRandom(max) {
-        let randomA = Math.floor(Math.random() * max);
-        while (randomA === this.number) {
-          randomA = Math.floor(Math.random() * max);
-              } 
-              this.number = randomA; 
-              return this.number; 
-            
-            } 
-            getRandom(16);
-            fields[this.number].append(img);  
-    }
+     let interval = setInterval(getRandom, 1000);
+  } 
 }
+
+
+
+
+
+
 
 
 
